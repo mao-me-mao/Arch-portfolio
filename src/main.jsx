6593d3d8f18c60639bad0a225150fa71a8e27fd8
sapphire <sapphire_pusa@yahoo.com>
2026-04-06 17:21:10 +0800

    made UI changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import "./styles/index.css";
 import "./styles/global.css";
 import App from "./App";
 import Shop from "./Shop";
-import Projects from "./Projects";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -13,7 +12,6 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/shop" element={<Shop />} />
-        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
